0cf4926b99a47623c2a4a577d094bbd81f97c2a1
moshiur111 <moshiur.code@gmail.com>
2026-01-12 20:32:39 +0600

    feat(db): add mongodb connection with mongoose

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import config from "./index";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
+    console.log("✅ Database connected successfully");
+  } catch (error) {
+    console.error("❌ Database connection failed");
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,7 @@ dotenv.config({
 const config = {
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || "development",
+  databaseUrl: process.env.DATABASE_URL,
 };
 
 export default config;

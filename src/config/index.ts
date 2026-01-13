60816ff09dd8f55eb5465946b9a9271b17d5c6c9
moshiur111 <moshiur.code@gmail.com>
2026-01-13 22:15:52 +0600

    feat(user): add base user model with roles, status, and security

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,7 @@ const config = {
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || "development",
   databaseUrl: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
 };
 
 export default config;
---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,76 @@
+import { model, Schema } from "mongoose";
+import { IUser, UserModel } from "./user.interface";
+import { USER_ROLES, USER_STATUS } from "./user.constant";
+import bcrypt from "bcrypt";
+import config from "../../config";
+
+const userSchema = new Schema<IUser, UserModel>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: Object.values(USER_ROLES),
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    passwordChangedAt: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: Object.values(USER_STATUS),
+        message: `Status must be one of: ${Object.values(USER_STATUS).join(
+          ", "
+        )}`,
+      },
+      default: USER_STATUS.IN_PROGRESS,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash password befor save
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+});
+
+// Static method for password comparison
+userSchema.statics.isPasswordMatched = async function (
+  plainTextPassword: string,
+  hashedPassword: string
+) {
+  return bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+export const User = model<IUser, UserModel>("User", userSchema);

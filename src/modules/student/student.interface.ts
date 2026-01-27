a3d82054ed7161cc0ada804c0a2029c406dc4aae
moshiur111 <moshiur.code@gmail.com>
2026-01-27 12:01:31 +0600

    feat(student): add student domain interface and model

---
+++ src/modules/student/student.interface.ts
@@ -0,0 +1,55 @@
+import { Types } from 'mongoose';
+
+export type TUserName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type TLocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type TGender = 'male' | 'female';
+
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export interface IStudent {
+  id: string;
+  user: Types.ObjectId;
+  name: TUserName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  profileImg?: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+  localGuardian: TLocalGuardian;
+  admissionSemester: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  isDeleted: boolean;
+}

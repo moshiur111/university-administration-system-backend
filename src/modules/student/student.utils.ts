7e5e2965c11b87d03884a27b0ea269a9dda8db0a
moshiur111 <moshiur.code@gmail.com>
2026-01-29 16:25:02 +0600

    feat: generate student id using year and semester code

--- src/modules/student/student.utils.ts
+++ src/modules/student/student.utils.ts
@@ -1,8 +1,12 @@
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { Student } from './student.model';
 
-const findLastStudentId = async () => {
+const findLastStudentId = async (
+  currentYear: string,
+  currentSemesterCode: string,
+) => {
   const lastStudent = await Student.findOne(
-    {},
+    { id: new RegExp(`^${currentYear}${currentSemesterCode}`) },
     { id: 1, _id: 0 },
   )
     .sort({ createdAt: -1 })
@@ -11,18 +15,29 @@ const findLastStudentId = async () => {
   return lastStudent?.id;
 };
 
-export const generateStudentId = async () => {
+export const generateStudentId = async (payload: IAcademicSemester) => {
   let currentId = '0';
 
-  const lastStudentId = await findLastStudentId();
+  const lastStudentId = await findLastStudentId(
+    payload.year.toString(),
+    payload.code,
+  );
 
-  if (lastStudentId) {
-    currentId = lastStudentId.split('-')[1];
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+
+  const currentYear = payload.year.toString();
+  const currentSemesterCode = payload.code;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
   }
 
-  const incrementId = (Number(currentId) + 1)
-    .toString()
-    .padStart(4, '0');
+  const incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
-  return `S-${incrementId}`;
+  return `${currentYear}${currentSemesterCode}${incrementId}`;
 };

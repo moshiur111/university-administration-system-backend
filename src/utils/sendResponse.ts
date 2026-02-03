1c378296a79d8208802d18ac2bd2442bf5219335
moshiur111 <moshiur.code@gmail.com>
2026-02-03 21:14:54 +0600

    refactor(core): enhance query builder and response utility

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -1,9 +1,17 @@
-import { Response } from "express";
+import { Response } from 'express';
+
+type TMeta = {
+  limit: number;
+  page: number;
+  total: number;
+  totalPages: number;
+};
 
 type TResponse<T> = {
   statusCode: number;
   success: boolean;
   message?: string;
+  meta?: TMeta;
   data: T;
 };
 
@@ -11,6 +19,7 @@ const sendResponse = <T>(res: Response, payload: TResponse<T>) => {
   res.status(payload.statusCode).json({
     success: payload.success,
     message: payload.message,
+    meta: payload.meta,
     data: payload.data,
   });
 };

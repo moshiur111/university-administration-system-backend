83d70b9ba024e06aeaa73d670a6d2600d3d138c9
moshiur111 <moshiur.code@gmail.com>
2026-02-08 18:45:27 +0600

    feat(upload): add image upload with cloudinary

--- src/modules/student/student.route.ts
+++ src/modules/student/student.route.ts
@@ -1,17 +1,44 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import auth from '../../middlewares/auth';
 import authorize from '../../middlewares/authorize';
 import { USER_ROLES } from '../users/user.constant';
 import validateRequest from '../../middlewares/validateRequest';
 import { StudentValidations } from './student.validation';
 import { StudentControllers } from './student.controller';
+import { upload } from '../../utils/sendImageToCloudinary';
 
 const router = Router();
+
+// router.post(
+//   '/create-student',
+//   auth(),
+//   authorize(USER_ROLES.ADMIN),
+//   upload.single('file'),
+//   (req:Request, res:Response, next:NextFunction) => {
+//     req.body = JSON.parse(req.body.data);
+//     next();
+//   },
+//   validateRequest(StudentValidations.createStudentValidationSchema),
+//   StudentControllers.createStudent,
+// );
 
 router.post(
   '/create-student',
   auth(),
   authorize(USER_ROLES.ADMIN),
+  upload.single('file'),
+
+  // ✅ reshape body for validation
+  (req: Request, res: Response, next: NextFunction) => {
+    if (req.body.data) {
+      const parsedData = JSON.parse(req.body.data);
+
+      req.body.student = parsedData.student;
+      req.body.password = parsedData.password;
+    }
+    next();
+  },
+
   validateRequest(StudentValidations.createStudentValidationSchema),
   StudentControllers.createStudent,
 );

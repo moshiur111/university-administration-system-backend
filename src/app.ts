3fb993ec0fa957abcbc1789d5d91841eeb728da9
moshiur111 <moshiur.code@gmail.com>
2026-04-09 16:43:24 +0600

    feat: add Vercel deployment configuration and update app entry point

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://frontend-domain.com',
+  'https://university-administration-system-fr.vercel.app',
 ];
 
 app.use(
